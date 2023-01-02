2a35cbe9672ddba8ace69d98db6314bcfc1e08cf
Martin Zahnd <mzahnd@itba.edu.ar>
2023-01-02 18:40:20 -0300

    Add paclan client

---
+++ paclan/run_client.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+SERVER_IP="192.168.8.140"
+SERVER_PORT=8095
+
+MIRRORLIST="/etc/pacman.d/mirrorlist"
+
+if ! curl -O "$SERVER_IP:$SERVER_PORT/mirrorlist"
+then
+    echo "Could not download mirrorlist"
+    exit 1
+fi
+
+sed -i "1s@^@Server = http://$SERVER_IP:$SERVER_PORT\n@" mirrorlist
+
+echo "Copying mirrorlist file to '$MIRRORLIST'"
+sudo cp mirrorlist "$MIRRORLIST"
+
+sudo pacman -Syy && \
+    sudo pacman -S -q --needed archlinux-keyring && \
+    sudo pacman -Su && \
+    yay --aur --batchinstall -Su
+
