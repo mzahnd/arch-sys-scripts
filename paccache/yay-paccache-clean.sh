d0cf7bea075bea1178cbcb2af317d19bf5c14f6a
Martín E. Zahnd <martin.zahnd@woocar.io>
2023-05-05 06:12:30 -0300

    Run beautysh

--- paccache/yay-paccache-clean.sh
+++ paccache/yay-paccache-clean.sh
@@ -4,14 +4,14 @@
 #
 # Copyright (c) 2021 Martín E. Zahnd < mzahnd at itba dot edu dot ar >
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights 
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-# copies of the Software, and to permit persons to whom the Software is 
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in 
+# The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -47,7 +47,7 @@ readonly KEEP_UNINSTALLED=0
 # Keep the latest version of installed packages in a specific user's yay cache.
 #
 # Given a user UID and its absolute home path, it will try to find .cache/yay/
-# directory and remove all but the latest $KEEP_UPDATED versions of all 
+# directory and remove all but the latest $KEEP_UPDATED versions of all
 # packages and the latest $KEEP_UNINSTALLED version of removed packages.
 #
 # Arguments
@@ -64,7 +64,7 @@ function yay_cache()
     local -r cache_root="${2}/.cache/yay/"
 
     [ ! -d "${cache_root}" ] && return 0
-    # paccache does not recurse into subdirectories.
+    # paccache does not recurses into subdirectories.
     # In order to get them listed, find gets all subdirectories in $cache_dir
     # and prints them with '--cachedir' before the path name (%p) in order to
     # pass this string directely to paccache.
@@ -72,18 +72,20 @@ function yay_cache()
     # starting point to stdout (the same could be achieved piping tail -n +2).
     #
     # WARNING: If you're going to edit the printf flag, keep the spaces around
-    #         to avoid printing something like 
+    #         to avoid printing something like
     #         '--cachedir /home/john/.cache/yay/yay--cachedir /home/ ... '
-    local -r cached_pkgs="$(find ${cache_root}   \
-        -maxdepth 1 ! -path ${cache_root}        \
+    local -r cached_pkgs="$(find "$cache_root"   \
+        -maxdepth 1 ! -path "$cache_root"        \
         -type d                                 \
         -printf ' --cachedir %p ')"
 
     # Quoting ${cached_pkgs} here will result in paccache interpreting
     # the string in an undesired way.
-    /usr/bin/paccache --verbose ${cached_pkgs} \
+    # shellcheck disable=2086
+    /usr/bin/paccache --verbose $cached_pkgs \
         --keep "${KEEP_UPDATED}" --remove
-    /usr/bin/paccache --verbose ${cached_pkgs} \
+    # shellcheck disable=2086
+    /usr/bin/paccache --verbose $cached_pkgs \
         --keep "${KEEP_UNINSTALLED}" --uninstalled --remove
 
     return 0
@@ -102,8 +104,8 @@ set -x
 #    # 'user%%:*' expands to get the UID
 #    # 'user##*:' expands to get the home path
 #    [ "${user%%:*}" -ge "1000" ] && \
-#        [[ ! "${SKIPPED_USERS[@]}" =~ "${username}" ]] && \
-#done < <(awk -v FS=':' '{printf "%s:%s:%s\n",$1,$3,$6;}' /etc/passwd)
+    #        [[ ! "${SKIPPED_USERS[@]}" =~ "${username}" ]] && \
+    #done < <(awk -v FS=':' '{printf "%s:%s:%s\n",$1,$3,$6;}' /etc/passwd)
 yay_cache "seabmo" "/home/seabmo"
 set +x
 
