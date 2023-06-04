542eb282a3f167ce165c750a49e42c89ebd0a9d3
Martín E. Zahnd <martin.zahnd@woocar.io>
2023-06-04 20:55:55 -0300

    Replace exclude as string for array

--- simplebckp/phone-bckp.sh
+++ simplebckp/phone-bckp.sh
@@ -5,29 +5,29 @@ readonly PHONE="/run/user/1000/gvfs/mtp:host=SAMSUNG_SAMSUNG_Android_R58RA11X8AJ
 
 # Relative to $PHONE
 phone_dirs=(\
-    "/Memoria interna/DCIM/Screenshots" \
-    "/Memoria interna/Documents" \
-    "/Memoria interna/Download" \
-    "/Memoria interna/Movies" \
-    "/Memoria interna/Pictures/Telegram" \
-    "/Memoria interna/Android/data/org.telegram.messenger/files/Pictures" \
-    "/Memoria interna/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Animated Gifs" \
-    "/Memoria interna/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Documents" \
-    "/Memoria interna/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Images" \
-    "/Memoria interna/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Video" \
-    "/Memoria interna (Doppio Account)/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Animated Gifs" \
-    "/Memoria interna (Doppio Account)/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Documents" \
-    "/Memoria interna (Doppio Account)/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Images" \
-    "/Memoria interna (Doppio Account)/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Video" \
+    "/Spazio di archiviazione interno/DCIM/Screenshots" \
+    "/Spazio di archiviazione interno/Documents" \
+    "/Spazio di archiviazione interno/Download" \
+    "/Spazio di archiviazione interno/Movies" \
+    "/Spazio di archiviazione interno/Pictures/Telegram" \
+    "/Spazio di archiviazione interno/Android/data/org.telegram.messenger/files/Pictures" \
+    "/Spazio di archiviazione interno/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Animated Gifs" \
+    "/Spazio di archiviazione interno/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Documents" \
+    "/Spazio di archiviazione interno/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Images" \
+    "/Spazio di archiviazione interno/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Video" \
+    "/Archiviazione interna (Doppio Account)/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Animated Gifs" \
+    "/Archiviazione interna (Doppio Account)/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Documents" \
+    "/Archiviazione interna (Doppio Account)/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Images" \
+    "/Archiviazione interna (Doppio Account)/Android/media/com.whatsapp/WhatsApp/Media/WhatsApp Video" \
     "/Scheda SD/DCIM" \
 )
 
-exclude_list=("Private/" "Sent/" "Obsidian/")
+exclude_list=("Private/" "Sent/" "Obsidian/" 'Documentos y digitalizaciones/' "Kee/")
 
-rsync_exclude=""
+rsync_exclude=()
 
 for exc in "${exclude_list[@]}"; do
-    rsync_exclude="${rsync_exclude} --exclude=${exc}"
+    rsync_exclude+=("--exclude=${exc}")
 done
 
 
@@ -50,6 +50,5 @@ if [ -n "$(ls -A "$OUTPUT")" ]; then
 fi
 
 for folder in "${phone_dirs[@]}"; do
-    rsync -va ${rsync_exclude} "${PHONE}/${folder}" "$OUTPUT"
+    rsync -va "${rsync_exclude[@]}" "${PHONE}/${folder}" "$OUTPUT"
 done
-
