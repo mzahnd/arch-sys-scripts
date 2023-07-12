6b935445fcece16027aa1d3bd56908b8f56fa287
Martin E. Zahnd <zahndme@gmail.com>
2023-07-12 12:40:39 -0300

    Replace ps+grep with pgrep

--- conky/conky-launch.sh
+++ conky/conky-launch.sh
@@ -5,14 +5,14 @@
 #
 # Copyright (c) 2021 Martín E. Zahnd < mzahnd at itba dot edu dot ar >
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights 
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-# copies of the Software, and to permit persons to whom the Software is 
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in 
+# The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -38,8 +38,8 @@
 # "${HOME}/.config/conky/calendar.conf"
 # "${HOME}/.config/conky/calendar-personal.conf"
 readonly CONFIGS=(\
-    "${HOME}/.config/conky/sys.conf"                \
-)
+        "${HOME}/.config/conky/sys.conf"                \
+    )
 
 # ====== END EDITABLE SECTION ======
 
@@ -69,9 +69,9 @@ readonly CONFIGS=(\
 #   1 : At least one Conky instance could not be killed.
 function kill_conky()
 {
-    local counter=0
+    # local counter=0
     local pid_list=""
-    local children=""
+    # local children=""
 
     # Terminate already running conky instances
     #killall --quiet 'conky' # Old way
@@ -80,32 +80,35 @@ function kill_conky()
     # very (!) important because 'kill' will try to _kill_ 'conky_launch' and
     # that's not what we want.
     # Then create a single-line space-separated string
-    pid_list=$(\
-        ps x -o "%p %c" | \
-            grep -iE '[[:alnum:]]+\ conky$' | \
-            awk '{print $1}' ORS=' ' 2> /dev/null \
-    )
+    # pid_list=$(\
+        #     ps x -o "%p %c" | \
+        #         grep -iE '[[:alnum:]]+\ conky$' | \
+        #         awk '{print $1}' ORS=' ' 2> /dev/null \
+        # )
 
-    # List 'conky' direct children if they exist
-    [ -n "$pid_list" ] && children=$(\
-        ps -o pid= --ppid $pid_list | awk '{print $1}' ORS=' ' 2> /dev/null \
-    )
+    # # List 'conky' direct children if they exist
+    # [ -n "$pid_list" ] && children=$(\
+        #     ps -o pid= --ppid $pid_list | awk '{print $1}' ORS=' ' 2> /dev/null \
+        # )
 
-    if [ -n "$children" ]; then
-       kill $children 
-       wait $children 2> /dev/null
-    fi
-    if [ -n "$pid_list" ]; then
-       kill $pid_list
-       wait $pid_list 2> /dev/null
-    fi
+    # if [ -n "$children" ]; then
+    #    kill $children
+    #    wait $children 2> /dev/null
+    # fi
+    # if [ -n "$pid_list" ]; then
+    #    kill $pid_list
+    #    wait $pid_list 2> /dev/null
+    # fi
+
+    pid_list=$(pgrep --euid "$USER" conky 2> /dev/null)
+    wait $pid_list
 
     return 0
 }
 
 # Launch a Conky instance in a specific monitor using a configuration file.
 #
-# If $CONKY_DEBUG is set (is not 0), it will launch it using '--debug' 
+# If $CONKY_DEBUG is set (is not 0), it will launch it using '--debug'
 # argument. When unset, it daemonizes the process using '--daemonize'.
 #
 # Arguments
@@ -121,9 +124,9 @@ function kill_conky()
 function launch_conky()
 {
     local args=(\
-        --xinerama-head="${1}"  \
-        --config="${2}"         \
-    )
+            --xinerama-head="${1}"  \
+            --config="${2}"         \
+        )
 
     [ "${CONKY_DEBUG}" ] && args+=('--debug') || args+=('--daemonize')
 
@@ -146,12 +149,12 @@ function start_conky()
     local return_code=0
     local -r nmonitors="$(xrandr --listactivemonitors | \
                             awk 'NR==1 {print $2}')"
-    
-    for (( i=0; i<${nmonitors}; i++)); do 
-       for config_file in "${CONFIGS[@]}"; do
+
+    for (( i=0; i<${nmonitors}; i++)); do
+        for config_file in "${CONFIGS[@]}"; do
             launch_conky "${i}" "${config_file}"
             return_code=$(( ${return_code} > ${?} ? ${return_code} : ${?} ))
-       done
+        done
     done
 
     return ${return_code}
@@ -159,7 +162,7 @@ function start_conky()
 
 # Main function.
 #
-# Tryies to kill old Conky instances and relaunches them in every connected 
+# Tryies to kill old Conky instances and relaunches them in every connected
 # monitor if succesfull.
 #
 # Arguments
